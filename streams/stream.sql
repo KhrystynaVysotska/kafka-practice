6e046b1e8f7f34537d33ed4b96a75a02801134eb
Khrystyna <khrystynavysotska@gmail.com>
2021-09-25 18:38:53 +0300

    Added new stream creation query and modified some previous

--- streams/stream.sql
+++ streams/stream.sql
@@ -1,5 +1,5 @@
--- stream that takes data from users topic where raw data is stored
-CREATE STREAM users_input (
+-- stream that takes raw data from customers topic
+CREATE STREAM customers_raw_data (
     email VARCHAR,
     first_name VARCHAR,
     last_name VARCHAR,
@@ -9,12 +9,12 @@ CREATE STREAM users_input (
     job VARCHAR,
     has_children_under_sixteen BOOLEAN
 ) WITH (
-    KAFKA_TOPIC='users',
+    KAFKA_TOPIC='customers',
     VALUE_FORMAT='AVRO'
 );
 
--- stateless stream that uses data from users_input (created above) and filters users by their age
-CREATE STREAM adult_users AS
+-- stateless stream that uses data from customers_raw_data (created above) and filters customers by their age
+CREATE STREAM adult_customers AS
     SELECT
         email,
         first_name,
@@ -24,16 +24,31 @@ CREATE STREAM adult_users AS
         gender,
         job,
         has_children_under_sixteen
-    FROM users_input
+    FROM customers_raw_data
     WHERE age >= 18
     EMIT CHANGES;
 
--- ktable that stores aggregated data about users: number of males and females in stream per minute
-CREATE TABLE count_users_by_gender AS
+-- ktable that stores aggregated data about customers: number of males and females in stream per minute
+CREATE TABLE amount_of_users_by_gender_for_the_last_minute AS
     SELECT
         gender,
         COUNT(gender) as count
-    FROM users_input
+    FROM customers_raw_data
     WINDOW TUMBLING (SIZE 1 MINUTE, GRACE PERIOD 2 HOURS)
     GROUP BY gender
+    EMIT CHANGES;
+
+-- stream that takes customers who have small children and may be potentially interested in buying toys
+CREATE STREAM customers_interesting_in_buying_toys AS
+    SELECT
+        email,
+        first_name,
+        last_name,
+        age,
+        address,
+        gender,
+        job,
+        has_children_under_sixteen
+    FROM customers_raw_data
+    WHERE has_children_under_sixteen=True
     EMIT CHANGES;
