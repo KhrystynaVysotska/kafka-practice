2901117997f6b5b2c001466ef97da5859d96d8a5
Khrystyna <khrystynavysotska@gmail.com>
2021-09-25 21:45:04 +0300

    Added queries for creating streams that work with product data

---
+++ streams/products_stream.sql
@@ -0,0 +1,33 @@
+-- stream that takes data from products topic
+CREATE STREAM products_raw_data (
+    barcode VARCHAR,
+    category VARCHAR,
+    name VARCHAR,
+    price VARCHAR,
+    description VARCHAR
+) WITH (
+    KAFKA_TOPIC = 'products',
+    VALUE_FORMAT = 'AVRO'
+);
+
+-- stream that filters stream of products raw data by category and selects only products from food category
+CREATE STREAM products_from_food_category AS
+    SELECT
+        barcode,
+        category,
+        name,
+        price,
+        description
+    FROM products_raw_data
+    WHERE category = 'Food'
+    EMIT CHANGES;
+
+-- ktable that stores amount of products of each category for the last five minutes with shifting every minute
+CREATE TABLE amount_of_products_by_category_for_the_last_five_minute AS
+    SELECT
+        category,
+        COUNT(category) as count
+    FROM products_raw_data
+    WINDOW HOPING (SIZE 5 MINUTES, ADVANCE BY 1 MINUTE)
+    GROUP BY category
+    EMIT CHANGES;
