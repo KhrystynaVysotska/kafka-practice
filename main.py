fae86e277987efc5c412e96bfb4996bddbb20c33
Khrystyna <khrystynavysotska@gmail.com>
2021-09-25 20:48:08 +0300

    Added function that produces product messages

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from entities.user import User
+from entities.product import Product
 from producer.user_producer import UserProducer
-from generator.user_generator import UserGenerator
+from producer.product_producer import ProductProducer
+from generator.entity_generator import EntityGenerator
 
 
 def produce_users():
     user_producer = UserProducer()
-    for users in UserGenerator.generate():
+    for users in EntityGenerator.generate(User):
         user_producer.send_records(users)
+
+
+def produce_products():
+    product_producer = ProductProducer()
+    for products in EntityGenerator.generate(Product):
+        product_producer.send_records(products)
 
 
 if __name__ == '__main__':
     produce_users()
+    produce_products()
